1541b112f2dc47a1e651de05cf1738e382ed7ae6
Natalia Galdoporpora <nataliagaldoporpora82@gmail.com>
2023-06-26 20:03:34 -0300

    react-intro

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card({name, status, species, gender, origin, image, onClose}) {
+
+   return (
+      <div>
+         <button onClick={onClose}>X</button>
+         <h2>Nombre: {name}</h2>
+         <h2>Status: {status}</h2>
+         <h2>Specie: {species}</h2>
+         <h2>Gender: {gender}</h2>
+         <h2>Origin: {origin}</h2>
+         <img src={image} alt = {`No se encuentra la imagen de ${name}`}/>
+      </div>
+   );
+}
